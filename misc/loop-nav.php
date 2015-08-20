13e13b65fe9273bd04d7a253d50ef2efcdac3aeb
leho <leho@kraav.com>
2015-08-20 21:30:20 +0300

    whitespace war: kill trailing

--- misc/loop-nav.php
+++ misc/loop-nav.php
@@ -8,10 +8,10 @@
 <?php elseif ( is_home() || is_archive() || is_search() ) : // If viewing the blog, an archive, or search results. ?>
 
 	<?php the_posts_pagination(
-		array( 
-			'prev_text' => esc_html_x( '&larr; Previous', 'posts navigation', 'hybrid-base' ), 
+		array(
+			'prev_text' => esc_html_x( '&larr; Previous', 'posts navigation', 'hybrid-base' ),
 			'next_text' => esc_html_x( 'Next &rarr;',     'posts navigation', 'hybrid-base' )
-		) 
+		)
 	); ?>
 
 <?php endif; // End check for type of page being viewed. ?>
